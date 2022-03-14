3f31de974b67c3412fc5b0eb41418e8e17f1cc23
Antony Butcher <antony.butcher@bristol.ac.uk>
2022-03-14 16:57:02 +0000

    seisan file reader added

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="seiscat",
+    version="0.0.1",
+    author="Antony Butcher",
+    description="A package for processing Distributed Acoustic Sensing (DAS) data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ seiscat/__init__.py
@@ -0,0 +1,7 @@
+name = "seiscat"
+__version__ = "0.0.1"
+__description__ = "Package for reading BGS catalogue files"
+__license__ = "MIT"
+__author__ = "Antony Butcher"
+__email__ = ""
+import seiscat.sfile_read
